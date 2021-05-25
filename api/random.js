ad42db55aea0ca119f0419d0c6188f46ad99bbfd
gloer <gloer@langslet.com>
2021-05-25 11:25:51 +0200

    Bugfix

--- api/random.js
+++ api/random.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
-    const rand = Math.ceil(Math.random() * 10)
-    res.staus(200).send(String(rand))
-}
+    const rand = Math.ceil(Math.random() * 10);
+    res.staus(200).send(String(rand));
+};
+  
